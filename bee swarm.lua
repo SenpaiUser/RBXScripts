512a2a229f4a3e772f48d74ca8b3bf5c4fb27436
SenpaiUser <89863034+SenpaiUser@users.noreply.github.com>
2021-09-28 20:01:56 +0300

    Create bee swarm.lua

---
+++ bee swarm.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SenpaiUser/RBXScripts/main/Trash.lua"))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Boxking776/RobloxScriptsDotCom/main/BeeSAutofarm.lua'),true))()
